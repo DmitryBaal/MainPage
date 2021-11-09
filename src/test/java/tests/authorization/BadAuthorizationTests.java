1a53fd518f409aa299dee9d23b61850fb06de2b9
Dmitry <Isidahate@gmail.com>
2021-11-09 18:59:54 +0300

    resrt

--- src/test/java/tests/authorization/BadAuthorizationTests.java
+++ src/test/java/tests/authorization/BadAuthorizationTests.java
@@ -1,15 +1,13 @@
 package tests.authorization;
 
 
+
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import tests.base.BaseTest;
-
 import static constants.Constants.AuthorizationTestData.AUTHORIZATION_LOGIN;
 import static constants.Constants.AuthorizationTestData.INVALID_PASSWORD;
-import static constants.Constants.ErrorsMessages.FIRST_AUTHORIZATION_FAIL;
-import static constants.Constants.ErrorsMessages.REG_EX_THIRD_AUTHORIZATION_FAIL;
-import static constants.Constants.ErrorsMessages.REG_EX_SECOND_AUTHORIZATION_FAIL;
+import static constants.Constants.ErrorsMessages.*;
 import static constants.Constants.Urls.AUTHORIZATION_PAGE_URL;
 
 
@@ -23,15 +21,13 @@ public class BadAuthorizationTests extends BaseTest {
                 .inputPass(INVALID_PASSWORD)
                 .selectButtonLogin()
                 .errorMsgField(FIRST_AUTHORIZATION_FAIL);
+
     }
 
     @Test
     @Order(2)
     public void checkErrorTwo() {
-        basepage.openUrl(AUTHORIZATION_PAGE_URL);
-        authorizationPage.clearLogin()
-                .inputLogin(AUTHORIZATION_LOGIN)
-                .inputPass(INVALID_PASSWORD)
+        authorizationPage.inputPass(INVALID_PASSWORD)
                 .selectButtonLogin()
                 .errorMsgField(FIRST_AUTHORIZATION_FAIL);
     }
@@ -39,25 +35,18 @@ public class BadAuthorizationTests extends BaseTest {
     @Test
     @Order(3)
     public void checkErrorThree() {
-      basepage.openUrl(AUTHORIZATION_PAGE_URL);
-      authorizationPage.clearLogin()
-                .inputLogin(AUTHORIZATION_LOGIN)
-                .inputPass(INVALID_PASSWORD)
+        authorizationPage.inputPass(INVALID_PASSWORD)
                 .selectButtonLogin()
-                .errorMsgExperiment(REG_EX_SECOND_AUTHORIZATION_FAIL);
+                .errorMsgField2(REG_EX_SECOND_AUTHORIZATION_FAIL);
     }
 
     @Test
     @Order(4)
     public void checkErrorFour() {
-        basepage.openUrl(AUTHORIZATION_PAGE_URL);
-        authorizationPage.clearLogin()
-                .inputLogin(AUTHORIZATION_LOGIN)
-                .inputPass(INVALID_PASSWORD)
+        authorizationPage.inputPass(INVALID_PASSWORD)
                 .selectButtonLogin()
-                .errorMsgExperiment(REG_EX_THIRD_AUTHORIZATION_FAIL);
+                .errorMsgField2(REG_EX_THIRD_AUTHORIZATION_FAIL);
     }
-
 
 
 }
